04c9c0ee2b36942fde0f2f267d34e287166609ea
mohamedemad-basha <144592551+mohamedemad-basha@users.noreply.github.com>
2026-08-02 01:23:07 +0300

    Clean up App.js by removing unused code
    Removed commented-out code related to state management and date calculations.

--- challenge-02-date-counter/src/App.js
+++ challenge-02-date-counter/src/App.js
@@ -1,4 +1,3 @@
-// import { useState } from "react";
 import Counter from "./Counter";
 export default function App() {
   return (
@@ -7,48 +6,4 @@ export default function App() {
     </div>
   );
 }
-//   const [step, setStep] = useState(1);
-//   const [count, setCount] = useState(0);
 
-//   const d = new Date();
-//   d.setDate(d.getDate() + count);
-
-//   const handleNextStep = function () {
-//     setStep((step) => step + 1);
-//   };
-
-//   const handlePrevStep = function () {
-//     if (step > 1) setStep((step) => step - 1);
-//   };
-
-//   const handleIncCount = function () {
-//     setCount((count) => count + step);
-
-//     // x = date.toDateString;
-//   };
-
-//   const handleDecCount = function () {
-//     setCount((count) => count - step);
-//     // setDate((date) => date - count);
-//     // x = date.toDateString;
-//   };
-
-//   // const date = new Date();
-//   // console.log(date);
-//   return (
-//     <div className="App">
-//       <div>
-//         <button onClick={handlePrevStep}>-</button>
-//         <span> Step: {step} </span>
-//         <button onClick={handleNextStep}>+</button>
-//       </div>
-
-//       <div>
-//         <button onClick={handleDecCount}>-</button>
-//         <span> Count: {count} </span>
-//         <button onClick={handleIncCount}>+</button>
-//       </div>
-//       <p>{d.toDateString()}</p>
-//     </div>
-//   );
-// }
